e1c1e612ec450226c13690d1dc729ba34aedc54a
shannah <steve@weblite.ca>
2012-10-29 11:59:45 -0700

    Added native

---
+++ native/libjcocoa/libjcocoa.h
@@ -0,0 +1,13 @@
+//
+//  libjcocoa.h
+//  libjcocoa
+//
+//  Created by Steve Hannah on 2012-10-21.
+//  Copyright (c) 2012 Web Lite Solutions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface libjcocoa : NSObject
+
+@end
---
+++ native/libjcocoaTests/libjcocoaTests.m
@@ -0,0 +1,32 @@
+//
+//  libjcocoaTests.m
+//  libjcocoaTests
+//
+//  Created by Steve Hannah on 2012-10-21.
+//  Copyright (c) 2012 Web Lite Solutions. All rights reserved.
+//
+
+#import "libjcocoaTests.h"
+
+@implementation libjcocoaTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in libjcocoaTests");
+}
+
+@end
