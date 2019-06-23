a60c0de67d14e7360a5ceaf8ee013ecd4924c9fb
Matthias Fuchs <matthias.fuchs@kps.com>
2019-06-23 09:33:46 +0200

    some exception refactoring

--- native/libjcocoa/WLJavaProxy.m
+++ native/libjcocoa/WLJavaProxy.m
@@ -39,8 +39,8 @@ static JavaVM *jvm = NULL;
         
         return self;
     } @catch (NSException *e) {
-        [JavaUtil throwJavaException: env withMessage: [[e reason] UTF8String] ];
         NSLog(@"Exception: %@", e);
+        [JavaUtil throwJavaException: env withMessage: [[e reason] UTF8String] ];
     }
 }
 
@@ -59,8 +59,8 @@ static JavaVM *jvm = NULL;
             //JNF_COCOA_EXIT(env);
         }
     } @catch (NSException *e) {
-        [JavaUtil throwJavaException: env withMessage: [[e reason] UTF8String] ];
         NSLog(@"Exception: %@", e);
+        [JavaUtil throwJavaException: env withMessage: [[e reason] UTF8String] ];
     }
 }
 
@@ -74,8 +74,8 @@ static JavaVM *jvm = NULL;
             jvm = theJvm;
         }
     } @catch (NSException *e) {
-        [JavaUtil throwJavaException: env withMessage: [[e reason] UTF8String] ];
         NSLog(@"Exception: %@", e);
+        [JavaUtil throwJavaException: env withMessage: [[e reason] UTF8String] ];
     }
 }
 
@@ -89,31 +89,38 @@ static JavaVM *jvm = NULL;
         if ( attach == 0 ){
             //JNF_COCOA_ENTER(env);
             signature = (NSMethodSignature*)(*env)->CallLongMethod(env, peer, jMethodSignatureForSelector, sel);
+            
+            if (!signature) {
+                signature = [@"" methodSignatureForSelector:sel];
+            }
+            
             //JNF_COCOA_EXIT(env);
         }
         //(*jvm)->DetachCurrentThread(jvm);
         return signature;
     } @catch (NSException *e) {
-        [JavaUtil throwJavaException: env withMessage: [[e reason] UTF8String] ];
         NSLog(@"Exception: %@", e);
+        [JavaUtil throwJavaException: env withMessage: [[e reason] UTF8String] ];
     }
 }
 
 -(void)forwardInvocation:(NSInvocation *)invocation
 {
     JNIEnv *env=0;
+    SEL aSelector = [invocation selector];
+    int attach = (*jvm)->AttachCurrentThread(jvm, (void**)&env, NULL);
     
     @try {
-        int attach = (*jvm)->AttachCurrentThread(jvm, (void**)&env, NULL);
-        if ( attach == 0 ){
+        if ( attach == 0 ) {
             //JNF_COCOA_ENTER(env);
             (*env)->CallVoidMethod(env, peer, jForwardInvocation, invocation);
             //JNF_COCOA_EXIT(env);
         }
         //(*jvm)->DetachCurrentThread(jvm);
     } @catch (NSException *e) {
-        [JavaUtil throwJavaException: env withMessage: [[e reason] UTF8String] ];
-        NSLog(@"Exception: %@", e);
+        NSString *message = [NSString stringWithFormat:@"Selector '%@' caused exception: %@", NSStringFromSelector(aSelector), e.reason];
+        NSLog(@"%@", message);
+        [JavaUtil throwJavaException: env withMessage: [message UTF8String] ];
     }
 }
 
@@ -132,8 +139,8 @@ static JavaVM *jvm = NULL;
         //(*jvm)->DetachCurrentThread(jvm);
         return response==1?TRUE:FALSE;
     } @catch (NSException *e) {
-        [JavaUtil throwJavaException: env withMessage: [[e reason] UTF8String] ];
         NSLog(@"Exception: %@", e);
+        [JavaUtil throwJavaException: env withMessage: [[e reason] UTF8String] ];
     }
 }
 
@@ -148,8 +155,30 @@ static JavaVM *jvm = NULL;
         }
         return NULL;
     } @catch (NSException *e) {
-        [JavaUtil throwJavaException: env withMessage: [[e reason] UTF8String] ];
         NSLog(@"Exception: %@", e);
+        [JavaUtil throwJavaException: env withMessage: [[e reason] UTF8String] ];
     }
 }
+
++ (NSSet<NSString *> *)keyPathsForValuesAffectingValueForKey:(NSString *)key {
+//    SEL theSelector = @selector(keyPathsForValuesAffectingValueForKey:);
+//    NSMethodSignature *aSignature = [NSObject methodSignatureForSelector:theSelector];
+//    NSInvocation *anInvocation = [NSInvocation invocationWithMethodSignature:aSignature];
+//
+//    [anInvocation setSelector:theSelector];
+//    [anInvocation setTarget:self];
+//    [anInvocation setArgument:&key atIndex:1];
+//
+//    [self forwardInvocation:anInvocation];
+//
+//    void *result;
+//    [anInvocation getReturnValue:result];
+//
+//    return result;
+    
+    
+    NSSet *keyPaths  = [[NSSet alloc] initWithObjects: key];
+    return keyPaths;
+}
+
 @end
