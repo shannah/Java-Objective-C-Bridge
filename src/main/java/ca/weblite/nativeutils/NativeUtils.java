937f48475bf5a485e4149347811491b7b5c0c93b
Marcono1234 <Marcono1234@users.noreply.github.com>
2021-01-10 18:44:38 +0100

    Simplify NativeUtils.loadFileFromJar(...)

--- src/main/java/ca/weblite/nativeutils/NativeUtils.java
+++ src/main/java/ca/weblite/nativeutils/NativeUtils.java
@@ -1,11 +1,12 @@
 package ca.weblite.nativeutils;
  
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
  
 /**
  * Simple library class for working with JNI (Java Native Interface)
@@ -45,10 +46,8 @@ public class NativeUtils {
      * @throws java.io.IOException if any.
      */
     public static void loadLibraryFromJar(String path, Class<?> source) throws IOException {
- 
-        
         // Finally, load the library
-        System.load(loadFileFromJar(path, source).getAbsolutePath());
+        System.load(loadFileFromJar(path, source).toAbsolutePath().toString());
     }
     
     /**
@@ -56,43 +55,37 @@ public class NativeUtils {
      *
      * @param path a {@link java.lang.String} object.
      * @param source a {@link java.lang.Class} object.
-     * @return a {@link java.io.File} object.
+     * @return file path of the temporary file extracted from this JAR
      * @throws java.io.IOException if any.
      */
-    public static File loadFileFromJar(String path, Class<?> source) throws IOException {
+    public static Path loadFileFromJar(String path, Class<?> source) throws IOException {
         if (!path.startsWith("/")) {
             throw new IllegalArgumentException("The path has to be absolute (start with '/').");
         }
  
-        // Obtain filename from path
-        String[] parts = path.split("/");
-        String filename = (parts.length > 1) ? parts[parts.length - 1] : null;
+        String filename = path.substring(path.lastIndexOf('/') + 1);
  
-        // Split filename to prexif and suffix (extension)
-        String prefix = "";
-        String suffix = null;
-        if (filename != null) {
-            parts = filename.split("\\.", 2);
-            prefix = parts[0];
-            suffix = (parts.length > 1) ? "."+parts[parts.length - 1] : null; // Thanks, davs! :-)
+        // Split filename to prefix and suffix (extension)
+        String prefix;
+        String suffix;
+        int lastDot = filename.lastIndexOf('.');
+        if (lastDot == -1) {
+            // No file extension; use complete filename as prefix 
+            prefix = filename;
+            suffix = null;
+        } else {
+            prefix = filename.substring(0, lastDot);
+            suffix = filename.substring(lastDot);
         }
  
         // Check if the filename is okay
-        if (filename == null || prefix.length() < 3) {
+        if (prefix.length() < 3) {
             throw new IllegalArgumentException("The filename has to be at least 3 characters long.");
         }
  
         // Prepare temporary file
-        File temp = File.createTempFile(prefix, suffix);
-        temp.deleteOnExit();
- 
-        if (!temp.exists()) {
-            throw new FileNotFoundException("File " + temp.getAbsolutePath() + " does not exist.");
-        }
- 
-        // Prepare buffer for data copying
-        byte[] buffer = new byte[1024];
-        int readBytes;
+        Path temp = Files.createTempFile(prefix, suffix);
+        temp.toFile().deleteOnExit();
  
         // Open and check input stream
         InputStream is = source.getResourceAsStream(path);
@@ -100,18 +93,9 @@ public class NativeUtils {
             throw new FileNotFoundException("File " + path + " was not found inside JAR.");
         }
  
-        // Open output stream and copy data between source file in JAR and the temporary file
-        OutputStream os = new FileOutputStream(temp);
-        try {
-            while ((readBytes = is.read(buffer)) != -1) {
-                os.write(buffer, 0, readBytes);
-            }
-        } finally {
-            // If read/write fails, close streams safely before throwing an exception
-            os.close();
-            is.close();
+        try (is; OutputStream out = Files.newOutputStream(temp, StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING)) {
+            is.transferTo(out);
         }
         return temp;
- 
     }
 }
